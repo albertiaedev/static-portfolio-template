b62c7bd5444e0da6e047f5bcfd9560a64160799c
Alberto <albertiae.digital@gmail.com>
2023-03-02 21:46:12 -0400

    Created a scroll down effect

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('.top');
+
+ScrollReveal().reveal('img');
+
+ScrollReveal().reveal('.navbar');
+
+ScrollReveal().reveal('.btn-download', { delay: 500 });
+
+ScrollReveal().reveal('.intro', { delay: 500 });
+
+ScrollReveal().reveal('.btn-hire', { delay: 500 });
+
+ScrollReveal().reveal('#about', { delay: 500 });
+
+ScrollReveal().reveal('#projects', { delay: 500 });
+
+ScrollReveal().reveal('#skills', { delay: 500 });
+
+ScrollReveal().reveal('#hire', { delay: 500 });
